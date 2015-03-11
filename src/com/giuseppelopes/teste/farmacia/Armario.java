f47ceb2d88168dab81933d9dce5f1140603618c6
Giuseppe Lopes <giuseppe.lopes@gmail.com>
2015-03-11 08:55:03 -0300

    Alterado o nome do pacote

--- src/com/giuseppelopes/teste/farmacia/Armario.java
+++ src/com/giuseppelopes/teste/farmacia/Armario.java
@@ -1,4 +1,4 @@
-package com.giuseppelopes;
+package com.giuseppelopes.teste.farmacia;
 
 import java.util.ArrayList;
 
--- src/com/giuseppelopes/teste/farmacia/FarmaciaTest.java
+++ src/com/giuseppelopes/teste/farmacia/FarmaciaTest.java
@@ -1,4 +1,4 @@
-package com.giuseppelopes;
+package com.giuseppelopes.teste.farmacia;
 
 public class FarmaciaTest {
 
